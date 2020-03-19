dfb6bb7247dbc2fa1698eb136f1b0cbf43019a22
Sam Jones <sam720_01@yahoo.com>
2020-03-19 18:44:59 -0500

    Navbar and footer from current site + Social link

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer bg-dark">
+  <div class="container">
+    <span class="text-muted">&copy 2019 Team Puma<br/>
+      <a href="https://www.metrostate.edu">Metropolitan State University</a><br/>
+  </div>
+</footer>
+<!-- Footer code sourced from https://github.com/VirtualScope/MajesticMagicians-Master/blob/master/Web/footer.php -->
