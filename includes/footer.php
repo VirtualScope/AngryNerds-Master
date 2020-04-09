a6cb6f0d319728af72e6360fe204ed13434b430c
kyle-hobbs <60247333+kyle-hobbs@users.noreply.github.com>
2020-04-08 21:01:31 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer bg-dark">
   <div class="container">
-    <span class="text-muted">&copy 2019 Team Puma<br/>
+    <span class="text-muted">&example text<br/>
       <a href="https://www.metrostate.edu">Metropolitan State University</a><br/>
   </div>
 </footer>
