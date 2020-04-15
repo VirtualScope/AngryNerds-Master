10e443868eb7c71ad40c4d31efed70ae8e52d924
kyle-hobbs <>
2020-04-15 01:40:34 -0500

    Fix minor issue with DB connection

--- includes/db_access_layer.php
+++ includes/db_access_layer.php
@@ -8,16 +8,10 @@ Class DatabaseAccessLayer {
     private $__db_passwd;
     private $__db_connection_status;
 
-    function __construct($db_host, $db_name, $db_user, $db_pass)
+    function __construct($db_host, $db_user, $db_pass, $db_name)
     {  
-        // Load DB config.
-        #DEFINE('DATABASE_HOST',     $db_host);
-        #DEFINE('DATABASE_DATABASE', $db_name);
-        #DEFINE('DATABASE_USER',     $db_user);
-        #DEFINE('DATABASE_PASSWORD', $db_pass);
-
         // Connect to DB
-        $this->__dbcn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
+        $this->__dbcn = new mysqli($db_host, $db_user, $db_pass, $db_name);
         $this->__dbcn->set_charset("utf8");
         if (mysqli_connect_errno()) {
             throw new Exception("Database Connection Error");
--- includes/db_config.php
+++ includes/db_config.php
@@ -3,8 +3,8 @@
 include("includes/db_access_layer.php");
 $database;
 // Create database connection object
-try{                                    #  Host        Database Name   Username Password
-    $database = new DatabaseAccessLayer("localhost", "angrynerdsmaster", "root", "");
+try{                                    #  Host     Username Password     Database Name 
+    $database = new DatabaseAccessLayer("localhost", "root", "", "angrynerdsmaster");
 } catch(Exception $e) 
 {
     echo ("<p> Unable to connect to database: " . $e->getMessage() . "</p>");
