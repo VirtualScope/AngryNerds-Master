0e5ff53bfa58125b63fcb11f617385e64044bff9
jschreifels <47926316+jschreifels@users.noreply.github.com>
2020-04-20 19:41:43 -0500

    db_access_layer fixes

--- includes/db_access_layer.php
+++ includes/db_access_layer.php
@@ -1,15 +1,13 @@
 <?php
 
-Class DatabaseAccessLayer {
+class DatabaseAccessLayer
+{
     // Properties
     private $__dbcn;
-    private $__db_host;
-    private $__db_user;
-    private $__db_passwd;
     private $__db_connection_status;
 
     function __construct($db_host, $db_user, $db_pass, $db_name)
-    {  
+    {
         // Connect to DB
         $this->__dbcn = new mysqli($db_host, $db_user, $db_pass, $db_name);
         $this->__dbcn->set_charset("utf8");
@@ -17,8 +15,7 @@ Class DatabaseAccessLayer {
             throw new Exception("Database Connection Error");
             $this->__db_connection_status = false;
             return;
-        } 
-        else 
+        } else
             $this->__db_connection_status = true;
     }
     function get_user_posts($courseId)
@@ -49,9 +46,9 @@ Class DatabaseAccessLayer {
         return $this->query($sql);
     }
     function get_classes($userId)
-    {
-        $sql = "SELECT course_id FROM user_course WHERE user_id=$userId";
-        return $this->query($sql);
+    {        
+        $sql = "SELECT course_id FROM user_course WHERE user_id=$userId";        
+        return $this->__dbcn->query($sql);
     }
     function get_course_database($courseId)
     {
@@ -74,6 +71,13 @@ Class DatabaseAccessLayer {
         VALUES (" . $userId . ", " . ($new_comment_post_id) . ", '" . ($new_comment_post_content) . "')";
         return $this->query($sql);
     }
-    private function query($sql)                      { return $this->__dbcn->query($sql); }
-    function get_database_connection_status() { return $this->__db_connection_status; }
+    private function query($sql)
+    {        
+        $result = $this->__dbcn->query($sql);
+        return $result;
+    }
+    function get_database_connection_status()
+    {
+        return $this->__db_connection_status;
+    }
 }
