31508e18864fa0aa9f59ad77b0134b6ee47bf115
jschreifels <47926316+jschreifels@users.noreply.github.com>
2020-04-25 10:32:37 -0500

    Forum comments default to expanded

--- Comments.php
+++ Comments.php
@@ -41,6 +41,7 @@ if (isset($_POST['new_comment_content']) && isset($_POST['new_comment_post_id'])
 
 <!DOCTYPE html>
 <html>
+<title>Forum</title>
 
 <body>
 
@@ -145,13 +146,13 @@ function displayUserPost($Database, $userPostId, $title, $content, $created_date
     
       <!-- Reveal Comments -->
       <p>
-        <a class="btn btn-outline-secondary" data-toggle="collapse" href="#commentSection' . $userPostId . '" role="button"  aria-controls="commentSection">
+        <a class="btn btn-outline-secondary" data-toggle="collapse" href="#commentSection' . $userPostId . '" role="button" aria-expanded="true" aria-controls="commentSection">
           Comments
         </a>
       </p>
     
       <!-- Comment -->
-      <div class="collapse in" id="commentSection' . $userPostId . '">';
+      <div class="collapse show in" id="commentSection' . $userPostId . '">';
 
   // ================= START Comments Section =================
 
