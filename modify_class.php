6d1e39be81eab7d7b1fb2b18558c086660696393
jschreifels <47926316+jschreifels@users.noreply.github.com>
2020-04-28 18:54:05 -0500

    Update modify_class.php

--- modify_class.php
+++ modify_class.php
@@ -82,28 +82,14 @@ if (isset($classCode) && isset($classDescription) && isset($userAssoc)) {
         }
     }
 
-    // Image processing.    
-    $fileName = basename($_FILES["fileToUpload"]["name"]);
-    $imageFileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
-    $targetFile = uniqid() . '.' . $imageFileType;
-    $fullPath = $webserver_root . 'images/' . $targetFile;
-
-    // Allow only certain file formats
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-        echo "<div class='text-center'>Invalid file format.</div>";
-        exit;
-    }
-
-    // Try to upload the file.
-    if (!move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $fullPath)) { # Replace uniqid to have picture name as filename.
-        echo "<div class='text-center'>An error was encountered while uploading the file.</div>";
-    }
-
     // If the user doesn't choose an image, keep the old image.
     if ($targetFile != "") {
 
-        // Image processing.
+        // Image processing.    
         $fileName = basename($_FILES["fileToUpload"]["name"]);
+        $imageFileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        $targetFile = uniqid() . '.' . $imageFileType;
+        $fullPath = $webserver_root . 'images/' . $targetFile;
 
         // Allow only certain file formats
         if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
